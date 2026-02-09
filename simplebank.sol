558c04acebda2397ad10f20aad354c58005d3e53
czzjcx <2556781158@qq.com>
2026-02-09 23:41:52 +0800

    feat: add whitelist with auto registration
    - Auto add users to whitelist when deposit reaches threshold
    - Owner can manually add users to whitelist
    - Improves user onboarding experience

--- simplebank.sol
+++ simplebank.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.20;
 contract SimpleBankWithEvent {
     address public owner; 
     mapping(address => uint256) public balances;
+    address[] public customerList;
+    mapping(address => bool) public hasRegistered;
+    uint256 public whitelistThreshold = 1 ether;
 
     event Deposited(address indexed user, uint256 amount);
+
     event Withdrawn(address indexed user, uint256 amount);
+
+    event CustomerRegistered(address indexed user);
 
     constructor() {
         owner = msg.sender;
@@ -20,6 +26,14 @@ contract SimpleBankWithEvent {
     function deposit() public payable {
         require(msg.value > 0, "No ETH sent");
         balances[msg.sender] += msg.value;
+        if (
+        balances[msg.sender] >= whitelistThreshold &&
+        !hasRegistered[msg.sender]
+    ) {
+        hasRegistered[msg.sender] = true;
+        customerList.push(msg.sender);
+        emit CustomerRegistered(msg.sender);
+    }
         emit Deposited(msg.sender, msg.value);
     }
 
@@ -38,5 +52,12 @@ contract SimpleBankWithEvent {
 
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+    function addToWhitelist(address user) public onlyOwner {
+    if (!hasRegistered[user]) {
+        customerList.push(user);
+        hasRegistered[user] = true;
+        emit CustomerRegistered(user);
+      }
     }
 }
